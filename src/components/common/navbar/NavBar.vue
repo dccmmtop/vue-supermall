0b4aa043b9766294e777f396b6cd2beda8f9cac3
dengchao <dengchao@zyb.com>
2020-02-26 18:40:51 +0800

    底部tab-bar

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+    <div class="left"> <solt name="left"></solt> </div>
+    <div class="center"> <solt name="center"></solt> </div>
+    <div class="right"> <solt name="right"></solt> </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
