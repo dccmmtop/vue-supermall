be23b1eedab4e3cecc0895c630d618ae2b82c371
dengchao <dengchao@zyb.com>
2020-02-27 12:01:24 +0800

    scroll

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,61 +3,84 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
-    <recommend-view  :recommends="recommends"></recommend-view>
-    <feature-view/>>
+    <scroll :probe-type="3" @scroll="contentScroll" @pulliingUp="loadMore" :pull-up-load="true" class="content">
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
+      <recommend-view  :recommends="recommends"></recommend-view>
+      <feature-view/>
+
+    </scroll>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+  // @ is an alias to /src
 
-import NavBar from "components/common/navbar/NavBar";
-import HomeSwiper from "views/home/childComps/HomeSwiper";
-import RecommendView from "views/home/childComps/RecommendView";
-import FeatureView from "views/home/childComps/FeatureView";
+  import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "views/home/childComps/HomeSwiper";
+  import RecommendView from "views/home/childComps/RecommendView";
+  import FeatureView from "views/home/childComps/FeatureView";
 
-import {getHomeMultiData} from "network/home";
+  import Scroll from "components/common/scroll/Scroll";
 
-export default {
-  name: 'Home',
-  components: {
-    NavBar,
-    HomeSwiper,
-    RecommendView,
-    FeatureView
-  },
-  data(){
-    return{
-      banners: [],
-      recommends: []
-    }
-  },
-  created() {
-    //1.请求多个数据
-    this.getHomeMultiData();
-  },
+  import {getHomeMultiData} from "network/home";
 
-  methods:{
-    swiperImageLoad(){},
+  export default {
+    name: 'Home',
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      Scroll
+    },
+    data(){
+      return{
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      //1.请求多个数据
+      this.getHomeMultiData();
+    },
 
-    getHomeMultiData() {
-      getHomeMultiData().then(res => {
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
+    methods:{
+      swiperImageLoad(){
+        // this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop;
+      },
 
-      }).catch(err => {
-        console.log(err)
-      });
-    }
-  },
+      getHomeMultiData() {
+        getHomeMultiData().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
 
-}
+        }).catch(err => {
+          console.log(err)
+        });
+      },
+
+      contentScroll(){
+
+      },
+      loadMore(){
+
+      }
+    },
+
+  }
 </script>
 <style scoped>
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
 
+  }
+  .content{
+    overflow: hidden;
+    position: absolute;
+    bottom: 49px;
+    top: 44px;
+    left: 0;
+    right: 0;
   }
 </style>
